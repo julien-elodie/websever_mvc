fa8b9842955b74898218505ceefae71bd2f79634
julien-elodie <2644756656@qq.com>
2017-11-07 00:52:52 +0800

    init project

--- public/js/loadtable.js
+++ public/js/loadtable.js
@@ -37,6 +37,7 @@ var TableInit = function () {
             showToggle:true,                            //是否显示详细视图和列表视图的切换按钮
             cardView: false,                            //是否显示详细视图
             detailView: false,                          //是否显示父子表
+            showPaginationSwitch:false,                  //是否显示数据条数选择框
             columns: [
                 {
                     checkbox: true
